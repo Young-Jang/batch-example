b972a768a85a220fd67d6e859bfa06d11b763af1
b116104@naver.com <b116104@naver.com>
2023-06-10 23:15:04 +0900

    feat: add testDept Repo

--- src/test/java/com/springbatch/example/domain/TestDeptRepository.java
+++ src/test/java/com/springbatch/example/domain/TestDeptRepository.java
@@ -5,17 +5,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class TestDeptRepository {
 
     @Autowired
     DeptRepository deptRepository;
 
+    private int testCount = 100;
+
     @Test
     @Commit
     public void dept01() {
-        for (int i = 1; i < 101; i++) {
+        for (int i = 1; i < testCount+1; i++) {
             deptRepository.save(new Dept(i, "dname_" + String.valueOf(i), "loc_" + String.valueOf(i)));
         }
+    }
+
+
+    @Test
+    @Commit
+    public void dept01_read() {
+        List<Dept> deptList = (List<Dept>) deptRepository.findAll();
+        assertEquals(deptList.size(),testCount);
     }
 }
